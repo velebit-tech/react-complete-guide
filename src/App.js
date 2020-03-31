32ea8a710db1d12debf1626701a5d037e445c8cc
Mario Markovic <velebit.business@gmail.com>
2020-03-31 17:55:04 +0200

    learning section 3: setState and useState hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    header_text: 'Hello Croatia',
+    people: [
+      { name: 'Mario', weight: 75 },
+      { name: 'Joso', weight: 700 },
+      { name: 'Karlo', weight: 80 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      people: [
+        { name: 'Mario Markovic', weight: 74 },
+        { name: 'Joso', weight: 700 },
+        { name: 'Karlo Markovic', weight: 81 }
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Hello Croatia</h1>
+        <h1>{this.state.header_text}</h1>
+        <button onClick={this.switchNameHandler}>Switch something</button>
+        <Person name={this.state.people[0].name} weight={this.state.people[0].weight} />
+        <Person name={this.state.people[1].name} weight={this.state.people[1].weight}>Also, I am fiction character.</Person>
+        <Person name={this.state.people[2].name} weight={this.state.people[2].weight} />
       </div>
     );
   }
