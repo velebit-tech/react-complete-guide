a0a41e4cacb42f4fdc95b831559ba6eef3c19253
Mario Markovic <velebit.business@gmail.com>
2020-04-03 12:02:52 +0200

    section 7: added auxiliary component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import Auxiliary from '../../hoc/Auxiliary';
 import './Person.css';
 
 const person = props => {
   return (
-    <div className="Person">
+    <Auxiliary>
       <p>I am {props.name} and I have {props.weight} kgs.</p>
       <p onClick={props.click}>{props.children}</p>
-      <input type="text" value={props.child} onChange={props.typing} /> 
-    </div>
+      <input type="text" value={props.child} onChange={props.typing} />
+    </Auxiliary>
   )
 }
 
